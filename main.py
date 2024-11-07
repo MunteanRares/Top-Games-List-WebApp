17684209220f2d79c2ea9b4a1dc9cce43f6c1317
ByatisGlaaki <rares_famuntean@yahoo.com>
2024-11-07 13:52:27 +0200

    insert

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column
@@ -19,12 +20,18 @@ db = SQLAlchemy(app)
 
 
 ###########################
-# CREATE CLASS FOR FORMS
+# CREATE CLASSES FOR FORMS
 ###########################
-class Form(FlaskForm):
-    rating = FloatField(label="Your Rating",)
+class UpdateForm(FlaskForm):
+    rating = FloatField(label="Your Rating", validators=[validators.DataRequired()])
     review = StringField(label="Your Review", validators=[validators.length(max=75)])
     submit = SubmitField(label="Submit")
+
+
+class AddGameForm(FlaskForm):
+    game_name = StringField(label="Game Name", validators=[validators.DataRequired()])
+    submit = SubmitField(label="Search")
+
 
 ###########################
 # CREATE FIRST TABLE named 'Games'
@@ -43,6 +50,59 @@ with app.app_context():
 
 
 ###########################
+# FUNCTIONS
+###########################
+### VALIDATE ON UPDATE PAGE
+def validate_update():
+    game_id_from_form = request.form.get("id")
+    new_review = request.form.get("review")
+    game_to_update = db.session.get(Game, game_id_from_form)
+    game_to_update.rating = request.form.get("rating")
+
+    if not new_review == "":
+        game_to_update.review = new_review
+    else:
+        print("GAME IS IS NULL")
+    db.session.commit()
+
+### REQUEST GAMES
+def get_games_list_data(search_value):
+    game_list = []
+    params = {
+        "key": "e8d6883d38f94755a3e56cdead8e9544",
+        "search": search_value
+    }
+
+    response_game = requests.get("https://api.rawg.io/api/games", params=params)
+    games_response = response_game.json()
+
+
+    for game in games_response["results"]:
+        game_list.append(game)
+    return game_list
+
+
+### SHORTEN DESCRIPTION
+def cut_paragraph(text):
+    cutoff = 250
+    end_position = text.rfind('.', 0, cutoff)
+
+    if end_position == -1:
+        shortened_text = text[:250]
+    else:
+        shortened_text = text[:end_position + 1]
+
+    return shortened_text
+
+
+### YEAR FROM DATE
+def get_year(date):
+    temp = date.split("-")
+    year = temp[0]
+    return year
+
+
+###########################
 # INSERT FIRST VALUES TO 'Game'
 ###########################
 new_game = Game(
@@ -56,11 +116,10 @@ new_game = Game(
                 " and prove it!",
     img_url="https://upload.wikimedia.org/wikipedia/en/0/0e/Euro_Truck_Simulator_2_cover.jpg"
 )
-# # Here we commit the changes
+# Here we commit the changes
 # with app.app_context():
 #     db.session.add(new_game)
 #     db.session.commit()
-
 
 ###########################
 # WEBSITE ROUTES
@@ -70,27 +129,18 @@ def home():
     all_games = db.session.query(Game).all()
     return render_template("index.html", all_games=all_games)
 
+
 @app.route('/edit', methods=["GET", "POST"])
 def update():
-    form = Form()
+    form = UpdateForm()
     game_id = request.args.get("id")
     game_to_update = db.session.get(Game, game_id)
 
     if form.validate_on_submit():
-        game_id_from_form = request.form.get("id")
-        new_review = request.form.get("review")
-
-        game_to_update = db.session.get(Game, game_id_from_form)
-        game_to_update.rating = request.form.get("rating")
-
-        if not new_review == "":
-            game_to_update.review = new_review
-        else:
-            print("GAME ID IS NULL")
-
-        db.session.commit()
+        validate_update()
         return redirect(location=url_for('home'))
     return render_template("update.html", form=form, game_to_update=game_to_update)
+
 
 @app.route("/delete")
 def delete():
@@ -100,6 +150,51 @@ def delete():
     db.session.delete(game_to_del)
     db.session.commit()
     return redirect(url_for('home'))
+
+
+@app.route("/add-game", methods=["GET", "POST"])
+def add_game():
+    form = AddGameForm()
+    all_games = db.session.query(Game).all()
+    if form.validate_on_submit():
+        game_name = request.form.get("game_name")
+        game_results = get_games_list_data(game_name)
+        print(game_results)
+    else:
+        game_results = []
+    return render_template("add_game.html", form=form, game_results=game_results, all_games=all_games)
+
+
+@app.route("/get_game")
+def get_game():
+    game_id = request.args.get("id")
+    params = {
+        "key": "e8d6883d38f94755a3e56cdead8e9544",
+    }
+    response = requests.get(f"https://api.rawg.io/api/games/{game_id}", params=params)
+
+    game_data = response.json()
+    print(game_data)
+
+    game_name = game_data["name"]
+    game_description = cut_paragraph(game_data["description_raw"])
+    release_date = get_year(game_data["released"])
+    img_background = game_data["background_image"]
+
+    game = Game(
+        rating=0,
+        review="None",
+        title=game_name,
+        year=release_date,
+        description=game_description,
+        img_url=img_background
+    )
+
+    with app.app_context():
+        db.session.add(game)
+        db.session.commit()
+        db.session.refresh(game)
+    return redirect(url_for('update', id=game.id))
 
 ###########################
 # RUN AND DEBUG SERVER
